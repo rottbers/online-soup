563ba8fb428accc3a5118f229cd5ca79136bcd61
rottbers <johan@rottbers.com>
2020-10-31 12:26:36 +0100

    Added basic page transition animation

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,9 +1,20 @@
+import { motion } from 'framer-motion';
+
 import s from './Layout.module.scss';
 
 interface Props {
   children: React.ReactNode;
 }
 
-const Layout: React.FC<Props> = ({ children }) => <div className={s.layout}>{children}</div>;
+const Layout: React.FC<Props> = ({ children }) => (
+  <motion.div
+    initial={{ opacity: 0 }}
+    animate={{ opacity: 1 }}
+    exit={{ opacity: 0 }}
+    className={s.layout}
+  >
+    {children}
+  </motion.div>
+);
 
 export default Layout;
